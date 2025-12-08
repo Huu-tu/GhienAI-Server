2d8638b04d9be49fde6be224a224b51b7469bf25
Huu_Tu <huutu7165@gmail.com>
2025-12-08 16:53:48 +0700

    big update

--- src/controllers/document.controller.ts
+++ src/controllers/document.controller.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from 'express'
 import Document from '../models/document.model'
 
-export const getAllDocument = ()=>{
-
+export const getAllDocument = async (req: Request, res: Response): Promise<void>=>{
+  try {
+    const documents = await Document.find()
+    if (documents && documents.length > 0) {
+      res.status(200).json(documents)
+    } else {
+      res.status(404).json({
+        message: 'No documents found.'
+      })
+    }
+  } catch (error: any) {
+    if (error.isJoi === true) {
+      res.status(400).json({
+        message: 'Invalid details provided.'
+      })
+    } else {
+      res.status(500).json({
+        message: 'An error occurred while fetching documents.',
+        error: error.message
+      })
+    }
+  }
 }
 
 export const uploadFileDocument = async (req: Request, res: Response): Promise<void> => {
@@ -11,7 +31,7 @@ export const uploadFileDocument = async (req: Request, res: Response): Promise<v
       res.status(400).json({ message: 'Document file is required.' })
       return
     }
-    const name = req.file.filename
+    const name = req.file.originalname
     const fileUrl = req.file.filename
     const fileType = req.file.mimetype
     const fileSize = req.file.size
