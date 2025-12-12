1c8f6cce9cab751127b359e130f3149f1ae82220
Huu_Tu <huutu7165@gmail.com>
2025-12-12 22:07:32 +0700

    dsad
    đâsd

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,10 @@ const port = Number(process.env.PORT) || 4000
 const url_server = process.env.url_server
 app.use(
   cors({
-    origin: true,
-    credentials: true
+    origin: ['http://103.90.227.169:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 )
 app.use(express.json({ limit: '30mb' }))
