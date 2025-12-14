a931ae72fd7f415bf45875a8749c74dc70456898
Huu_Tu <huutu7165@gmail.com>
2025-12-14 20:08:03 +0700

    c

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,7 @@ const app = express()
 
 const port = Number(process.env.PORT) || 4000
 const url_server = process.env.url_server
-app.use(
-  cors({
-    origin: 'http://103.90.227.169:3000',
-    credentials: false
-  })
-)
+app.use(cors());
 app.use(express.json({ limit: '30mb' }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use('/img', express.static(path.join(__dirname, 'publics/img')))
